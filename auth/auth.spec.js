345ee229ce49e438eb754f0db50dbef6099a7ada
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-21 16:21:26 -0400

    few bugs

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -31,6 +31,24 @@ describe("test server enpoints for register and login", () => {
 
       expect(res.status).toBe(200);
     });
+
+    it("should return 500 if username is not unique", async () => {
+      const res = await request(server).post("/api/auth/register").send({
+        username: "marko",
+        password: "polo",
+      });
+
+      expect(res.status).toBe(500);
+    });
+
+    it("should return 2 when successful register", async () => {
+      const res = await request(server).post("/api/auth/register").send({
+        username: "markopolo",
+        password: "prostofire",
+      });
+
+      expect(res.body[0]).toBe(2);
+    });
   });
 
   describe("POST /login", () => {
@@ -43,13 +61,31 @@ describe("test server enpoints for register and login", () => {
       expect(res.status).toBe(200);
     });
 
-    it("should return 404 with wrong password", async () => {
+    it("should return 401 with wrong password", async () => {
       const res = await request(server).post("/api/auth/login").send({
         username: "marko",
         password: "polos",
       });
 
       expect(res.status).toBe(401);
+    });
+
+    it("wrong password should return invalid", async () => {
+      const res = await request(server).post("/api/auth/login").send({
+        username: "marko",
+        password: "polos",
+      });
+
+      expect(res.body.message).toBe("invalid");
+    });
+
+    it("should not return 401 with correct password", async () => {
+      const res = await request(server).post("/api/auth/login").send({
+        username: "marko",
+        password: "polo",
+      });
+
+      expect(res.status).not.toBe(401);
     });
   });
 });
