d957f99bd1117b1baa78b2f64f9d620e14b13547
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-08-21 13:19:48 -0400

    cleaned up

--- users/user-model.js
+++ users/user-model.js
@@ -2,16 +2,11 @@ const db = require("../database/dbConfig");
 
 module.exports = {
   add,
-  remove,
   findBy,
 };
 
 function add(user) {
   return db("users").insert(user);
-}
-
-function remove(id) {
-  return db("users").where({ id }).del();
 }
 
 function findBy(filter) {
